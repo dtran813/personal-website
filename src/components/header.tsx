f11ca8830725fb85521b65d8ee82f10e2f9ff8ca
dtran <dtran13899@gmail.com>
2024-11-02 22:44:52 -0600

    Reformat files

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,15 +2,14 @@ import Link from "next/link";
 import { navLinks } from "@/utils/constant";
 
 const Header = () => (
-  <nav className="p-4 h-auto">
-    <div className="container mx-auto flex justify-between items-center">
-      <Link href="/" className="text-white font-bold text-2xl">
+  <nav className="p-4">
+    <div className="container mx-auto flex items-center justify-between">
+      <Link href="/" className="text-2xl font-bold text-white">
         Hoa Tran
       </Link>
-      <div className="space-x-8 lg:space-x-12 text-lg">
-        {
-          navLinks.map(({id, title}) => (
-            <Link key={id} href={`#{id}`} className="text-white">
+      <div className="space-x-8 text-lg lg:space-x-12">
+        {navLinks.map(({ id, title }) => (
+          <Link key={id} href={`#${id}`} className="text-white">
             {title}
           </Link>
         ))}
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,20 +2,20 @@ import SplitText from "@/components/UI/splitText";
 
 const Hero = () => (
   <section id="hero" className="h-screen">
-    <div className="relative flex items-center justify-center h-full text-center">
+    <div className="relative flex h-full items-center justify-center text-center">
       <div className="mb-32">
         <h1
-          className="name text-4xl md:text-6xl text-white font-bold"
+          className="name text-4xl font-bold text-white md:text-6xl"
           role="heading"
           aria-label="Hoa Tran"
           aria-level={1}
         >
           <SplitText text={"Hoa Tran"} />
         </h1>
-        <p className="text-xl md:text-2xl text-gray-300 mt-4">
+        <p className="mt-4 text-xl text-gray-300 md:text-2xl">
           I am a Full Stack Developer & Designer
         </p>
-        <button className="mt-6 px-6 py-3 bg-blue-500 text-white font-bold rounded">
+        <button className="mt-6 rounded bg-blue-500 px-6 py-3 font-bold text-white">
           Learn More About Me
         </button>
       </div>
